65ae473f7820fc1d44fbd27978d0b3fe4c51a9a1
JoakinRoa151 <joakin.roa@usach.cl>
2023-09-21 00:49:43 -0300

    controladores

---
+++ mrp-be/src/main/java/manriquezrivera/proyecto/controllers/MateriaController.java
@@ -0,0 +1,12 @@
+package manriquezrivera.proyecto.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@CrossOrigin
+public class MateriaController {
+    
+}
