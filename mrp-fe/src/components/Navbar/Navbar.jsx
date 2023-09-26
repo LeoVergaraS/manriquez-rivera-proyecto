e4c31896d31f91c184316f780105b2a94ef45fca
LeoVergaraS <leo.vergara@usach.cl>
2023-09-26 01:00:51 -0300

    estilos header completo

--- mrp-fe/src/components/Navbar/Navbar.jsx
+++ mrp-fe/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import './Navbar.css'
+import './navbar.scss'
 
 function NavbarResponsive() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="m-auto justify-content-center gap-3" >
-                        <Nav.Link href="/">Inicio</Nav.Link>
-                        <Nav.Link href="/consulta">Tiempo de caso</Nav.Link>
+        <Navbar expand="lg" className="navbar bg-body-tertiary">
+            <Container className='navbar__container'>
+                <Navbar.Toggle className="navbar__toggle" aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className="navbar__collapse" id="basic-navbar-nav">
+                    <Nav className="navbar__nav m-auto justify-content-center gap-3" >
+                        <Nav.Link className="navbar__nav-link" href="/">Inicio</Nav.Link>
+                        <Nav.Link className="navbar__nav-link" href="/consulta">Tiempo de caso</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
