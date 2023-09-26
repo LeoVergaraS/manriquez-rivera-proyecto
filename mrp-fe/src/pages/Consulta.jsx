6481635a4ed07c5cf47c6450856f1b80932d583c
Luis Toro <luis.toro.f@usach.cl>
2023-09-26 01:53:56 -0300

    home parte 1

--- mrp-fe/src/pages/Consulta.jsx
+++ mrp-fe/src/pages/Consulta.jsx
@@ -1,6 +1,6 @@
 const Consulta = () => {
     return (
-        <h1>Mi primera pega</h1>
+        <h1>Consulta mi primera pega</h1>
     )
 }
 export default Consulta
--- mrp-fe/src/pages/Home.jsx
+++ mrp-fe/src/pages/Home.jsx
@@ -1,6 +1,33 @@
-const Home = () => {
+import Form from 'react-bootstrap/Form';
+import { Container, Col, Row } from 'react-bootstrap';
+import { BiSearchAlt } from 'react-icons/bi';
+import './home.scss'
+
+function Home() {
     return (
-        <h1>Home</h1>
-    )
+        <Container>
+            <Row style={{ margin: "75px", alignItems:"center" }}>
+                <Col xs="auto">
+                    <BiSearchAlt style={{ color: "white", fontSize: "35px" }} />
+                </Col>
+                <Col>
+                    <Form.Select aria-label="Default select example">
+                        <option>¿Qué cliente desea buscar?</option>
+                        <option value="1">Juan Iturbe</option>
+                        <option value="2">Eduardo Abarca</option>
+                        <option value="3">Miki Carcamo</option>
+                    </Form.Select>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+
+                </Col>
+            </Row>
+
+        </Container>
+    );
 }
-export default Home
+
+export default Home;
