c438b7191177c2fc21dd055e4ac1fbd633f05c4a
JoakinRoa151 <joakin.roa@usach.cl>
2023-09-26 03:28:57 -0300

    consulta parte 1

--- mrp-fe/src/pages/Consulta.jsx
+++ mrp-fe/src/pages/Consulta.jsx
@@ -1,6 +1,22 @@
+import Form from 'react-bootstrap/Form';
+import { Container, Col, Row } from 'react-bootstrap';
+import { BiSearchAlt } from 'react-icons/bi';
 const Consulta = () => {
     return (
-        <h1>Consulta mi primera pega</h1>
+        <Container>
+        <Row style={{ margin: "75px", alignItems:"center" , justifyContent: "center" }}>
+            <Col xs={2}>
+                <Form.Select aria-label="Default select example">
+                    <option>Cliente</option>
+                </Form.Select>
+            </Col>
+            <Col xs={2}>
+                <Form.Select aria-label="Default select example">
+                    <option>Materia</option>
+                </Form.Select>
+            </Col>
+        </Row>
+        </Container>
     )
 }
 export default Consulta
