b4f41b5435bd02b673785c87420c0b3be9f78854
LeoVergaraS <leo.vergara@usach.cl>
2023-09-27 01:41:01 -0300

    feat: controlador de submateria

--- mrp-be/src/main/java/manriquezrivera/proyecto/controllers/SubmateriaController.java
+++ mrp-be/src/main/java/manriquezrivera/proyecto/controllers/SubmateriaController.java
@@ -1,12 +1,55 @@
 package manriquezrivera.proyecto.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import manriquezrivera.proyecto.entity.Submateria;
+import manriquezrivera.proyecto.services.SubMateriaService;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @RestController
 @CrossOrigin
+@RequestMapping("/submaterias")
 public class SubmateriaController {
-    
+    @Autowired
+    SubMateriaService submateriaService;
+
+    @GetMapping
+    public ResponseEntity<List<Submateria>> getSubmaterias(){
+      List<Submateria> submaterias = submateriaService.getAllSubMaterias();
+      if(submaterias == null){
+        return ResponseEntity.notFound().build();
+      }
+      return ResponseEntity.ok().body(submaterias);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Submateria> getSubmateria(@PathVariable(value = "id") Long id){
+      Submateria submateria = submateriaService.getSubMateriaById(id);
+      if(submateria == null){
+        return ResponseEntity.notFound().build();
+      }
+      return ResponseEntity.ok().body(submateria);
+    }
+
+    @PostMapping
+    public ResponseEntity<Submateria> postSubmateria(@RequestBody Submateria submateria){
+      Submateria submateriaGuardada = submateriaService.saveSubMateria(submateria);
+      return ResponseEntity.ok().body(submateriaGuardada);
+    }
+
+    @PostMapping("/delete/{id}")
+    public ResponseEntity<String> deleteSubmateria(@PathVariable(value = "id") Long id){
+      submateriaService.deleteSubMateriaById(id);
+      return ResponseEntity.ok().body("Submateria eliminada correctamente");
+    }
 }
