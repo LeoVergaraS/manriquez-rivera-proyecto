9ceba43ac364b13517d27e0fa1f7fd31fa526dfb
LeoVergaraS <leo.vergara@usach.cl>
2023-10-07 00:01:46 -0300

    cronometro (falta el reset)

--- mrp-fe/src/components/Cronometro/Cronometro.jsx
+++ mrp-fe/src/components/Cronometro/Cronometro.jsx
@@ -1,90 +1,115 @@
 import { useEffect, useState, useRef } from "react";
 import "./cronometro.scss";
-import { FaPlayCircle, FaRegPlayCircle } from 'react-icons/fa';
-import { FaCirclePause } from 'react-icons/fa6';
-import { VscDebugRestart } from 'react-icons/vsc';
-import { GiSaveArrow } from 'react-icons/gi'
+import { FaPlayCircle, FaRegPlayCircle } from "react-icons/fa";
+import { FaCirclePause } from "react-icons/fa6";
+import { VscDebugRestart } from "react-icons/vsc";
+import { GiSaveArrow } from "react-icons/gi";
 
 function Cronometro() {
-    const [diff, setDiff] = useState(0)
-    const [initial, setInitial] = useState(0)
-    const [isPlaying, setIsPlaying] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
 
-    const animationRef = useRef(null);
+  const tiempoInicial = parseInt(localStorage.getItem("tiempoCronometro")) || 0;
+  const [tiempo, setTiempo] = useState(tiempoInicial);
+  const intervalRef = useRef(null);
 
-    const togglePlay = () => {
-        setIsPlaying(!isPlaying);
-    };
+  // Función para formatear el tiempo en formato HH:mm:ss
+  const formatearTiempo = (tiempo) => {
+    const horas = String(Math.floor(tiempo / 3600)).padStart(2, "0");
+    const minutos = String(Math.floor((tiempo % 3600) / 60)).padStart(2, "0");
+    const segundos = String(tiempo % 60).padStart(2, "0");
+    return `${horas}:${minutos}:${segundos}`;
+  };
 
-    const tick = () => {
-        setDiff(new Date().getTime() - initial);
-        animationRef.current = requestAnimationFrame(tick);
-    };
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+    setIsPaused(false);
+  };
 
-    const start = () => {
-        if(!isPlaying){
-          togglePlay();
-          setInitial(new Date().getTime() - diff);
-          animationRef.current = requestAnimationFrame(tick);
-        }
+  const start = () => {
+    if (!isPlaying) {
+      togglePlay();
+
+      intervalRef.current = setInterval(() => {
+        setTiempo((prevTiempo) => {
+          const nuevoTiempo = prevTiempo + 1;
+          localStorage.setItem("tiempoCronometro", String(nuevoTiempo));
+          return nuevoTiempo;
+        });
+      }, 1000);
     }
+  };
+  
 
-    const pause = () => {
-        if(isPlaying){
-          togglePlay();
-          cancelAnimationFrame(animationRef.current);
-        }
+  const pause = () => {
+    if (isPlaying) {
+      togglePlay();
+      setIsPaused(true);
+      clearInterval(intervalRef.current);
     }
+  };
 
-    const save = () => {
-        console.log("save");
+  const save = () => {
+    console.log("save");
+  };
+
+  // Función para reiniciar el cronómetro
+  const reset = () => {
+    if(isPlaying) {
+      togglePlay();
+      setIsPaused(true);
     }
+    setTiempo(0);
+    localStorage.setItem("tiempoCronometro", "00:00:00");
+  };
 
-    const restart = () => {
-      if(isPlaying){
-        pause();
-      }
-      setInitial(0)
-      setDiff(0)
-    }
 
-    useEffect(() => {
-        return () => {
-          cancelAnimationFrame(animationRef.current);
-        }
-    }, []);
+  // Actualizar el tiempo cada segundo
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setTiempo((prevTiempo) => {
+//         const nuevoTiempo = prevTiempo + 1;
+//         localStorage.setItem("tiempoCronometro", String(nuevoTiempo));
+//         return nuevoTiempo;
+//       });
+//     }, 1000);
+//     return () => clearInterval(interval);
+//   }, []);
 
-    return (
-        <div className="App">
-            <h1 className="timer">{timeFormat(diff)}</h1>
-            <div style={{ display: "flex", justifyContent: "center", alignItems:"center", marginTop: "20px", gap: "10px" }}>
-                <GiSaveArrow onClick={save} style={{ cursor: "pointer", color: "#DFBF68", fontSize:"40px" }} />
-                {isPlaying ? (
-                    <FaCirclePause onClick={pause} style={{ cursor: "pointer", color: "#DFBF68", fontSize: "65px" }} />
-                ) : (
-                    <FaPlayCircle onClick={start} style={{ cursor: "pointer", color: "#DFBF68", fontSize: "65px" }} />
-                )}
-                <VscDebugRestart onClick={restart} style={{ cursor: "pointer", color: "#DFBF68", fontSize:"40px"}}/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1 className="timer">{formatearTiempo(tiempo)}</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "20px",
+          gap: "10px",
+        }}
+      >
+        <GiSaveArrow
+          onClick={save}
+          style={{ cursor: "pointer", color: "#DFBF68", fontSize: "40px" }}
+        />
+        {isPlaying ? (
+          <FaCirclePause
+            onClick={pause}
+            style={{ cursor: "pointer", color: "#DFBF68", fontSize: "65px" }}
+          />
+        ) : (
+          <FaPlayCircle
+            onClick={start}
+            style={{ cursor: "pointer", color: "#DFBF68", fontSize: "65px" }}
+          />
+        )}
+        <VscDebugRestart
+          onClick={reset}
+          style={{ cursor: "pointer", color: "#DFBF68", fontSize: "40px" }}
+        />
+      </div>
+    </div>
+  );
 }
-
-const timeFormat = (milliseconds) => {
-    console.log(milliseconds)
-    if (!milliseconds) return "00:00:00";
-
-    const totalSeconds = Math.floor(milliseconds / 1000);
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
-
-    const hh = hours < 10 ? "0" + hours : hours;
-    const mm = minutes < 10 ? "0" + minutes : minutes;
-    const ss = seconds < 10 ? "0" + seconds : seconds;
-
-    return `${hh}:${mm}:${ss}`;
-};
-
 
 export default Cronometro;
--- mrp-fe/src/pages/Youtube.jsx
+++ mrp-fe/src/pages/Youtube.jsx
@@ -1,25 +1,57 @@
 import { Container } from "react-bootstrap";
 import "./youtube.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Youtube = () => {
   const [selectedCliente, setSelectedCliente] = useState("");
-  const [selectedMateria, setSelectedMateria] = useState("");
+  const [selectedGeneral, setSelectedGeneral] = useState("");
+  const [selectedMateria, setSelectedMateria] = useState("selected");
+  const [consultasC, setConsultasC] = useState([]);
 
   const handleSelected = (type) => {
     if (type === "cliente") {
       setSelectedCliente("selected");
       setSelectedMateria("");
+      setSelectedGeneral("");
     } else if (type === "materia") {
       setSelectedMateria("selected");
       setSelectedCliente("");
+      setSelectedGeneral("");
+    } else {
+      setSelectedGeneral("selected");
+      setSelectedCliente("");
+      setSelectedMateria("");
     }
   };
+
+  const getConsultasMateria = async () => {
+    try {
+      let url = "http://localhost:8090/consultas/materia";
+      const response = await axios.get(url);
+      if (response.status === 200) {
+        setConsultasC(response.data);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getConsultasMateria();
+  }, []);
 
   return (
     <div className="layout">
       <div className="navegador">
+        <button onClick={() => console.log(consultasC)}>Click</button>
         <ul className="navegador__tabs">
+          <p
+            className={"navegador__tabs-item " + selectedGeneral}
+            onClick={() => handleSelected("general")}
+          >
+            General
+          </p>
           <p
             className={"navegador__tabs-item " + selectedCliente}
             onClick={() => handleSelected("cliente")}
@@ -43,10 +75,21 @@ const Youtube = () => {
               <p className="tabla__cabecera-item">Tiempo</p>
             </div>
           </div>
+          {consultasC.map((consulta) => {
+            return (
+              <div className="tabla__row">
+                <div className="tabla__content">
+                  <div className="tabla__content-item">{consulta.nombre}</div>
+                  <div className="tabla__content-item">{consulta.tiempo}</div>
+                </div>
+              </div>
+            );
+          })}
+
           <div className="tabla__row">
-            <div className="tabla__content">
-              <div className="tabla__content-item">Miki</div>
-              <div className="tabla__content-item">Miki</div>
+            <div className="tabla__footer">
+              <div className="tabla__footer-item">Total</div>
+              <div className="tabla__footer-item">Total</div>
             </div>
           </div>
         </div>
