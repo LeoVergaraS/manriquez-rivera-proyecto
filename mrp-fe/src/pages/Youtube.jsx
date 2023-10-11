43660c04f593a13266fe4868d7fcd28606b1f77a
LeoVergaraS <leo.vergara@usach.cl>
2023-10-11 02:12:07 -0300

    personalizado funcionando a medias (XD)

--- mrp-fe/src/pages/Youtube.jsx
+++ mrp-fe/src/pages/Youtube.jsx
@@ -1,4 +1,4 @@
-import { Container, Form } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 import "./youtube.scss";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -8,6 +8,12 @@ const Youtube = () => {
   const [selectedCliente, setSelectedCliente] = useState("");
   const [selectedGeneral, setSelectedGeneral] = useState("");
   const [selectedMateria, setSelectedMateria] = useState("selected");
+  const [personalizado, setPersonalizado] = useState({
+    fechaInicio: new Date(),
+    fechaFin: new Date(),
+  });
+
+  const [showModal, setShowModal] = useState(false);
 
   const [consultasC, setConsultasC] = useState([]);
   const [consultasM, setConsultasM] = useState([]);
@@ -19,30 +25,56 @@ const Youtube = () => {
   const headerMateria = ["Materia", "Tiempo"];
   const headerClientes = ["Fecha", "Tiempo"];
 
+  const handleModal = () => setShowModal(!showModal);
+
   const handleSelected = (type) => {
     if (type === "cliente") {
       setSelectedCliente("selected");
       setSelectedMateria("");
       setSelectedGeneral("");
-      
     } else if (type === "materia") {
       setSelectedMateria("selected");
       setSelectedCliente("");
       setSelectedGeneral("");
-      
     } else {
       setSelectedGeneral("selected");
       setSelectedCliente("");
       setSelectedMateria("");
-      
     }
   };
 
-  const getConsultasMateria = async (filtro) => {
+  const getFechaFin = (fi, filtro) => {
+    if (filtro === 0) {
+      return new Date(fi.getFullYear(), fi.getMonth(), fi.getDate() - 7);
+    } else if (filtro === 1) {
+      return new Date(fi.getFullYear(), fi.getMonth() - 1, fi.getDate());
+    } else if (filtro === 2) {
+      return new Date(fi.getFullYear(), fi.getMonth() - 3, fi.getDate());
+    } else if (filtro === 3) {
+      return new Date(fi.getFullYear(), fi.getMonth() - 6, fi.getDate());
+    } else if (filtro === 4) {
+      return new Date(fi.getFullYear() - 1, fi.getMonth(), fi.getDate());
+    } else {
+      return new Date();
+    }
+  };
+
+  const getConsultasMateria = async (
+    filtro,
+    fechaInicio = null,
+    fechaFin = null
+  ) => {
     try {
-      const ff = new Date();
-      const fi = getFechaFin(ff, filtro);
-      let url = "http://localhost:8090/consultas/materia/" + formatDate(fi) + "/" + formatDate(ff);
+      let ff, fi;
+      fechaFin === null ? (ff = new Date()) : (ff = new Date(fechaFin));
+      fechaInicio === null
+        ? (fi = getFechaFin(ff, filtro))
+        : (fi = new Date(fechaInicio));
+      let url =
+        "http://localhost:8090/consultas/materia/" +
+        formatDate(fi) +
+        "/" +
+        formatDate(ff);
       const response = await axios.get(url);
       if (response.status === 200) {
         setConsultasM(response.data);
@@ -55,58 +87,71 @@ const Youtube = () => {
   const handleSelect = (e) => {
     setCliente(e.target.value);
     getConsultasCliente(e.target.value, filtro);
-  }
-  
+  };
+
+  const handleSearch = () => {
+    getConsultasMateria(
+      filtro,
+      personalizado.fechaInicio,
+      personalizado.fechaFin
+    );
+    getConsultasCliente(
+      cliente,
+      filtro,
+      personalizado.fechaInicio,
+      personalizado.fechaFin
+    );
+    handleModal();
+  };
+
   const handleFiltro = (filtro) => {
     setFiltro(filtro);
-    if(selectedCliente === "selected") getConsultasCliente(cliente, filtro);
-    if(selectedMateria === "selected") getConsultasMateria(filtro);
-  }
-
-  const getFechaFin = (fi, filtro) => {
-    if(filtro === 0){
-      return new Date(fi.getFullYear(), fi.getMonth(), fi.getDate() - 7);
-    }else if(filtro === 1){
-      return new Date(fi.getFullYear(), fi.getMonth() - 1, fi.getDate());
-    }else if(filtro === 2){
-      return new Date(fi.getFullYear(), fi.getMonth() - 3, fi.getDate());
-    }else if(filtro === 3){
-      return new Date(fi.getFullYear(), fi.getMonth() - 6, fi.getDate());
-    }else if(filtro === 4){
-      return new Date(fi.getFullYear() - 1, fi.getMonth(), fi.getDate());
-    }else{
-        return new Date();
-      }
-  }
+    getConsultasCliente(cliente, filtro);
+    getConsultasMateria(filtro);
+  };
 
   const formatDate = (date) => {
     let dd = date.getDate();
     let mm = date.getMonth() + 1;
     let yyyy = date.getFullYear();
-    if(dd < 10){
-      dd = '0' + dd;
+    if (dd < 10) {
+      dd = "0" + dd;
     }
-    if(mm < 10){
-      mm = '0' + mm;
+    if (mm < 10) {
+      mm = "0" + mm;
     }
-    return yyyy + '-' + mm + '-' + dd;
-  }
+    return yyyy + "-" + mm + "-" + dd;
+  };
 
-  const getConsultasCliente = async (cliente, filtro) => {
+  const getConsultasCliente = async (
+    cliente,
+    filtro,
+    fechaInicio = null,
+    fechaFin = null
+  ) => {
     try {
-      const ff = new Date();
-      const fi = getFechaFin(ff, filtro);
-      let url = "http://localhost:8090/consultas/cliente/" + cliente + "/" + formatDate(fi) + "/" + formatDate(ff);
+      let ff, fi;
+      fechaFin === null ? (ff = new Date()) : (ff = new Date(fechaFin));
+      fechaInicio === null
+        ? (fi = getFechaFin(ff, filtro))
+        : (fi = new Date(fechaInicio));
+      let url =
+        "http://localhost:8090/consultas/cliente/" +
+        cliente +
+        "/" +
+        formatDate(fi) +
+        "/" +
+        formatDate(ff);
       const response = await axios.get(url);
       if (response.status === 200) {
-        console.log(response.data)
+        console.log(response.data);
         setConsultasC(response.data);
       }
     } catch (err) {
       console.error(err.message);
     }
   };
-  
+
   const getClientes = async () => {
     try {
       let url = "http://localhost:8090/clientes";
@@ -117,7 +162,7 @@ const Youtube = () => {
     } catch (err) {
       console.error(err.message);
     }
-  }
+  };
 
   useEffect(() => {
     getConsultasMateria(filtro);
@@ -125,53 +170,128 @@ const Youtube = () => {
   }, []);
 
   return (
-    <div className="layout">
-      <div className="navegador">
-        <ul className="navegador__tabs">
-          <p
-            className={"navegador__tabs-item " + selectedGeneral}
-            onClick={() => handleSelected("general")}
-          >
-            General
-          </p>
-          <p
-            className={"navegador__tabs-item " + selectedCliente}
-            onClick={() => handleSelected("cliente")}
-          >
-            Clientes
-          </p>
-          <p
-            className={"navegador__tabs-item " + selectedMateria}
-            onClick={() => handleSelected("materia")}
-          >
-            Materia
-          </p>
-        </ul>
+    <>
+      <div className="layout">
+        <div className="navegador">
+          <ul className="navegador__tabs">
+            <p
+              className={"navegador__tabs-item " + selectedGeneral}
+              onClick={() => handleSelected("general")}
+            >
+              General
+            </p>
+            <p
+              className={"navegador__tabs-item " + selectedCliente}
+              onClick={() => handleSelected("cliente")}
+            >
+              Clientes
+            </p>
+            <p
+              className={"navegador__tabs-item " + selectedMateria}
+              onClick={() => handleSelected("materia")}
+            >
+              Materia
+            </p>
+          </ul>
+        </div>
+
+        <div className="main">
+          {/*selectedGeneral*/}
+          {selectedCliente === "selected" ? (
+            <div
+              className="d-flex align-items-center"
+              style={{ flexDirection: "column" }}
+            >
+              <Form.Select
+                value={cliente}
+                onChange={handleSelect}
+                style={{ fontSize: 25, width: "20%", marginTop: 20 }}
+              >
+                <option value={0} key={0}>
+                  Seleccione un cliente
+                </option>
+                {clientes.map((cliente) => (
+                  <option value={cliente.id} key={cliente.id}>
+                    {cliente.nombre}
+                  </option>
+                ))}
+              </Form.Select>
+              <Table_01 header={headerClientes} listObject={consultasC} />
+            </div>
+          ) : null}
+          {selectedMateria === "selected" ? (
+            <Table_01 header={headerMateria} listObject={consultasM} />
+          ) : null}
+          <aside className="filtro">
+            <div className="filtro__column">
+              <div className="filtro__item" onClick={() => handleFiltro(0)}>
+                1 semana
+              </div>
+              <div className="filtro__item" onClick={() => handleFiltro(1)}>
+                1 mes
+              </div>
+              <div className="filtro__item" onClick={() => handleFiltro(2)}>
+                3 meses
+              </div>
+              <div className="filtro__item" onClick={() => handleFiltro(3)}>
+                6 meses
+              </div>
+              <div className="filtro__item" onClick={() => handleFiltro(4)}>
+                1 año
+              </div>
+              <div className="filtro__item" onClick={handleModal}>
+                Personalizado
+              </div>
+            </div>
+          </aside>
+        </div>
       </div>
 
-      <div className="main">
-        {/*selectedGeneral*/}
-        {selectedCliente === "selected" ? 
-          <div className="d-flex align-items-center" style={{flexDirection:"column"}}>
-            <Form.Select value={cliente} onChange={handleSelect} style={{fontSize: 25, width: "20%", marginTop: 20}}>
-              <option value={0} key={0}>Seleccione un cliente</option>
-              {clientes.map((cliente) => <option value={cliente.id} key={cliente.id}>{cliente.nombre}</option>)}
-            </Form.Select>
-            <Table_01 header={headerClientes} listObject={consultasC} />
-          </div> : null}
-        {selectedMateria === "selected" ? <Table_01 header={headerMateria} listObject={consultasM} /> : null}
-        <aside className="filtro">
-          <div className="filtro__column">
-            <div className="filtro__item" onClick={() => handleFiltro(0)}>1 semana</div>
-            <div className="filtro__item" onClick={() => handleFiltro(1)}>1 mes</div>
-            <div className="filtro__item" onClick={() => handleFiltro(2)}>3 meses</div>
-            <div className="filtro__item" onClick={() => handleFiltro(3)}>6 meses</div>
-            <div className="filtro__item" onClick={() => handleFiltro(4)}>1 año</div>
-            <div className="filtro__item" onClick={() => handleFiltro(5)}>Personalizado</div>
-          </div>
-        </aside>
-      </div>
-    </div>
+      <Modal show={showModal} onHide={handleModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Rango de tiempo</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Fecha inicio</Form.Label>
+              <Form.Control
+                type="date"
+                value={formatDate(personalizado.fechaInicio)}
+                onChange={(e) =>
+                  setPersonalizado({
+                    ...personalizado,
+                    fechaInicio: new Date(e.target.value),
+                  })
+                }
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Fecha fin</Form.Label>
+              <Form.Control
+                type="date"
+                value={formatDate(personalizado.fechaFin)}
+                onChange={(e) =>
+                  setPersonalizado({
+                    ...personalizado,
+                    fechaFin: new Date(e.target.value),
+                  })
+                }
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleModal}>
+            Cancelar
+          </Button>
+          <Button variant="success" onClick={handleSearch}>
+            Buscar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
