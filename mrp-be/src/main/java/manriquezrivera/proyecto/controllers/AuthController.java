2e0d1354f6f4de2802f927c93685b718e53c3bab
Luis Toro <luis.toro.f@usach.cl>
2023-10-30 01:58:46 -0300

    iniciando login

---
+++ mrp-be/src/main/java/manriquezrivera/proyecto/controllers/AuthController.java
@@ -0,0 +1,19 @@
+package manriquezrivera.proyecto.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor    
+public class AuthController {
+    
+    @PostMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+}
